3b52b97c6567f8cd4712379429b6433ab0433ea0
miniz233 <zhiminzhang.zzm@gmail.com>
2026-08-01 09:01:58 +0000

    Adapt page for Motion Beyond Morphology paper

--- fix_syntax.py
+++
@@ -1,6 +0,0 @@
-with open("index.html", "r") as f:
-    html = f.read()
-
-html = html.replace('});\n    </script>', '});\n        });\n    </script>')
-with open("index.html", "w") as f:
-    f.write(html)
--- towebp.sh
+++
@@ -1,9 +0,0 @@
-find /Users/mengzijie/Downloads/project/KlingCameraControl.github.io/camera \
-  -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) | \
-while IFS= read -r f; do
-  cwebp -q 80 "$f" -o "${f%.*}.webp"
-done
-
-# find /Users/mengzijie/Downloads/project/KlingCameraControl.github.io/camera \
-#   -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -delete
-
